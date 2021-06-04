bd7c13cdc31bda2d462ee42a983554d555b9906c
Mohsin Sheikh <sheikhmohsin706@gmail.com>
2021-06-04 20:51:27 +0500

    Minor changes

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -3,35 +3,46 @@ import { Spin } from "antd";
 import firebase from "firebase/app";
 import router from "next/router";
 import React, { useEffect, useState } from "react";
-import { DefaultUser } from "../components/GlobalUserContext";
+import { isSameUser, PrototypeUser } from "../components/GlobalUserContext";
 import Header from "../components/header";
 import SettingsComponent from "../components/SettingsComponent";
 import initializeFirebase from "../services/firebase";
 
 const Settings = () => {
   const [isChecking, setIsChecking] = useState(true);
-  let [user, setUser] = useState(DefaultUser);
+  let [user, setUser] = useState(PrototypeUser);
 
   const checkLoginSession = () => {
     initializeFirebase();
 
-    firebase.auth().onAuthStateChanged((currentUser: firebase.User) => {
-      if (currentUser) {
-        var username = currentUser.displayName
-          ? currentUser.displayName
-          : currentUser.uid.slice(0, 5);
+    if (user === PrototypeUser) {
+      firebase.auth().onAuthStateChanged((currentUser: firebase.User) => {
+        if (currentUser) {
+          var username = currentUser.displayName
+            ? currentUser.displayName
+            : currentUser.uid.slice(0, 5);
 
-        setUser({
-          email: currentUser.email,
-          displayName: username,
-          id: currentUser.uid,
-          photoURL: currentUser.photoURL,
-        });
-        setIsChecking(false);
-      } else {
-        router.push("../login");
-      }
-    });
+          const cUser = {
+            email: currentUser.email,
+            displayName: username,
+            id: currentUser.uid,
+            photoURL: currentUser.photoURL,
+          };
+
+          if (!isSameUser(cUser, user)) {
+            setUser({
+              email: currentUser.email,
+              displayName: username,
+              id: currentUser.uid,
+              photoURL: currentUser.photoURL,
+            });
+          }
+          setIsChecking(false);
+        } else {
+          router.push("../login");
+        }
+      });
+    }
   };
 
   // This validates the login session at component load.
